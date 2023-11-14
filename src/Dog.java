3e4f679c4dbf63888c82cfcfa1cdbe9aa0ed048f
Atticus Blanco <atticusbrujx@gmail.com>
2023-11-14 12:52:46 -0800

    Part 2 >>>NONOPERATIONAL
    >>>MODIFICATIONS
    --CREATED Dog.java
      -ADDED psuedo
    
    >>>DEFICITS
    --
    
    >>>BUGS
    --
    
    >>>THROWS
    --

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,10 @@
 public class Dog {
+
+//    You should have private instance variables/fields for all the data members.
+//    You should have a constructor that sets the initial state of the data members via passed parameters.
+//    Think about what data-types the fields and parameters will need!!!
+//    You should have 4 instance methods for eat, run, sleep, and name
+//    You should have a main method that creates the 3 Dog objects in the diagram.
+//    Make at least 2 of your methods functional (i.e. perform some action)!!!
+//    For Example: method eat might take a class parameter named Food (i.e. another class) that has a field weight, and your eat method might reduce the weight in Food.
 }
