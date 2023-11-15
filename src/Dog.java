b9b19495e3aa57cf7ebfff522554cd58a64b0730
Atticus Blanco <atticusbrujx@gmail.com>
2023-11-14 21:29:30 -0800

    Part 3 >>>OPERATIONAL
    >>>MODIFICATIONS
    --NEAT TRICK: Lab-009: Crack.java(line 32) -> Crypt.class(line 22) -> Sha2Crypt.class
      -(line 56); I followed the documentation, and got to reading about the Pattern and Matcher classes. From what I can tell, the SALT_PATTERN Pattern object (Line 24) compiled capture groups, defined which were optional and what could be in each group. I took that same idea, created my own pattern that looks for specific string patterns instead of capture groups. I found on Line 56 that a Matcher object m can be made from call the matcher method on the SALT_PATTERN pattern object passing the salt value.
      -In my code, I created a class variable temp that works as an ad-lib. The Pattern object compiles all the String values that will be replaced. The while loop continues while the boolean matcher.find() returns true. The string stringMatch is the value of a matched string identified in the Pattern object pattern, which in turn is used as the conditional variable for the switch tree. Each case is a possible match, which then uses a StringBuffer object to buffer the temp string until a match is found, the match is replaced with the instance variable associated with the Dog object passed to the method. Then the StringBuffer moves to the next match, replaces, and continues until all matches are replaced. The .appendTail method buffs the remaining of the temp string that does not contain a match. the StringBuffer is then printed to terminal. Holy cow. That was cool.
    >>>DEFICITS
    --Generate a .txt file with ad-lib prompts. Refer to Lab-008 to implement a BufferedReader object, and refer to Lab-005 to impliment the Random object. Build a method that selects a random line from a BufferedReader object, and returns that random line as a String that can, in turn, be passed to the blurb method in place of the temp String.
    
    >>>BUGS
    --
    
    >>>THROWS
    --C:\Users\Gnomatic\IdeaProjects\Java-Assignment-010\src\Dog.java
      Warning:(10, 20) Field 'name' may be 'final'
      Warning:(11, 20) Field 'breed' may be 'final'
      Warning:(12, 20) Field 'size' may be 'final'
      Warning:(13, 22) Field 'color' may be 'final'
      Warning:(14, 17) Field 'age' may be 'final'
      Warning:(15, 27) Field can be converted to a local variable
      Warning:(15, 27) Field 'printTemplate' may be 'final'
      Warning:(17, 27) Field can be converted to a local variable
      Warning:(17, 27) Field 'temp' may be 'final'
      Warning:(101, 13) Switch statement can be replaced with enhanced 'switch'
      Warning:(125, 17) Method 'eat()' is never used
      Warning:(126, 9) 'return' is unnecessary as the last statement in a 'void' method
      Warning:(128, 17) Method 'run()' is never used
      Warning:(129, 9) 'return' is unnecessary as the last statement in a 'void' method
      Warning:(131, 17) Method 'sleep()' is never used
      Warning:(132, 9) 'return' is unnecessary as the last statement in a 'void' method
      Warning:(134, 17) Method 'name()' is never used
      Warning:(135, 9) 'return' is unnecessary as the last statement in a 'void' method

--- src/Dog.java
+++ src/Dog.java
@@ -1,28 +1,31 @@
-import java.lang.reflect.Array;
+//import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Dog {
 
     private String name;
     private String breed;
+    private String size;
     private String[] color;
     private int age;
     private static String printTemplate =
             "Name: %s%nAge: %d%nBreed: %s%nColor: %s%n";
+    private static String temp = "My service dog is a SIZE BREED named NAME. He is AGE year old, and has COLOR fur.";
 
 
-//    You should have a constructor that sets the initial state of the data members via passed parameters.
-    public Dog (String name, int age, String breed, String[] color) {
+    public Dog (String name, int age, String size, String breed, String[] color) {
+
         this.name = name;
         this.age = age;
+        this.size = size;
         this.breed = breed;
         this.color = color;
 
         System.out.printf(printTemplate, name, age, breed, getColorString(color));          //scaffold
-
-        return;
     }
 
     private static String getName() {
@@ -32,10 +35,8 @@ public class Dog {
     }
 
     private static int getAge() {
-
         Scanner s = new Scanner(System.in);
         int age;
-
         do {
             System.out.print("Enter your dog's age: ");
             while (!s.hasNextInt()) {
@@ -44,9 +45,17 @@ public class Dog {
             }
             age = s.nextInt();
         } while (age <= 0);
-
         return age;
-//        return 5;
+    }
+
+    public static String getSize() {
+        String[] arraySize= {"Small", "Medium", "Large"};
+        System.out.println("Enter size");
+        Scanner s = new Scanner(System.in);
+        for (int i = 0; i < arraySize.length; i++){               //for loop copied from getNumbers method::Assignment009
+            System.out.printf("%d: \"%s\"%n", i + 1, arraySize[i]);
+        }
+        return arraySize[Integer.parseInt(s.nextLine()) - 1];          //modify to checksum the input
     }
 
     public static String getBreed(){
@@ -59,7 +68,6 @@ public class Dog {
         List<String> color = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         String input;
-
         System.out.println("Enter the colors of the dog (type 'F' to finish):");
         do {
             input = scanner.nextLine();
@@ -70,9 +78,8 @@ public class Dog {
         return color.toArray(new String[0]);
     }
 
-    public static String getColorString(String[] color) {
+    public static String getColorString(String[] color) {       //What is the significance of @notNull???
         StringBuilder colorString = new StringBuilder();
-        int colorSize = color.length;
         for (int i = 0; i < color.length; i++) {
             colorString.append(color[i]);
             if (i < color.length-2) {
@@ -82,6 +89,37 @@ public class Dog {
             }
         }
         return colorString.toString();
+    }
+
+    public static void blurb(String temp, Dog dog) {
+        Pattern pattern = Pattern.compile("(NAME|BREED|SIZE|COLOR|AGE)");
+        Matcher matcher = pattern.matcher(temp);
+
+        StringBuffer output = new StringBuffer();
+        while (matcher.find()) {
+            String stringMatch = matcher.group();
+            switch (stringMatch) {
+                case "NAME":
+                    matcher.appendReplacement(output, dog.name);
+                    break;
+                case "BREED":
+                    matcher.appendReplacement(output, dog.breed);
+                    break;
+                case "SIZE":
+                    matcher.appendReplacement(output, dog.size);
+                    break;
+                case "COLOR":
+                    matcher.appendReplacement(output, getColorString(dog.color));
+                    break;
+                case "AGE":
+                    matcher.appendReplacement(output, String.valueOf(dog.age));
+                    break;
+                default:
+                    break;
+            }
+        }
+        matcher.appendTail(output);
+        System.out.println(output);
     }
 
     public void eat() {
@@ -100,13 +138,13 @@ public class Dog {
     //    You should have a main method that creates the 3 Dog objects in the diagram.
     public static void main(String[] args) {
 
-        Dog dog1 = new Dog(getName(), getAge(), getBreed(), getColor());
+        Dog dog1 = new Dog(getName(), getAge(), getSize(), getBreed(), getColor());
 
-        Dog dog2 = new Dog(getName(), getAge(), getBreed(), getColor());
+//        Dog dog2 = new Dog(getName(), getAge(), getSize(), getBreed(), getColor());
 
-        Dog dog3 = new Dog(getName(), getAge(), getBreed(), getColor());
-        return;
+//        Dog dog3 = new Dog(getName(), getAge(), getSize(), getBreed(), getColor());
+
+        blurb(temp, dog1);
+
     }
-//    Make at least 2 of your methods functional (i.e. perform some action)!!!
-//    For Example: method eat might take a class parameter named Food (i.e. another class) that has a field weight, and your eat method might reduce the weight in Food.
 }
