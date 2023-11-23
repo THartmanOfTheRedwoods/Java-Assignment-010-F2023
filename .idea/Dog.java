dedcaccea25b483b40d32bdba4e6c41a1a0e26ba
FinnCl4rk <phinphan11@gmail.com>
2023-11-23 11:26:13 -0800

    Completed Assignment 10

--- .idea/Dog.java
+++ .idea/Dog.java
@@ -1,49 +1,97 @@
 import java.util.ArrayList;
-
 public class Dog {
     private String breed;
-    public String size;
+    private String size;
     private String color;
     private int age;
-    public Dog Dog1Object;
-    public Dog Dog2Object;
-    public Dog Dog3Object;
-  public static ArrayList<String> dogs = new ArrayList<>();
+    public Dog[] dogs;
+    public static String food;
+    public static String exercise;
+    public static String shutEye;
+    public static String collar;
 
     public Dog(String breed, String size, String color, int age){
         this.breed = breed;
         this.size = size;
         this.color = color;
         this.age = age;
+        //this.dogs = dogs;
     }
-    public String eat(String size){
-        Food eaten = new Food();
-        eaten.eat();
+
+    public String getBreed(){
+        return breed;
+    }
+    public String getSize(){
         return size;
     }
-    public void run (Distance exercise){
-
+    public String getColor(){
+        return color;
     }
-    public void sleep(SleepDuration hours){
-
+    public int getAge(){
+        return age;
     }
-    public void name(Collar inspect){
-
+    public static void eat(Dog[] dogs) {
+        for(Dog d : dogs){
+            if (d.getSize().equals("Small")){
+                food = "1 cup of food";
+            }if (d.getSize().equals("Medium")){
+                food = "3 cups of food";
+            }if (d.getSize().equals("Large")){
+                food = "5 cups of food";
+            }
+            System.out.printf("Dog %s has eaten %s\n", d.getBreed(), food);
+        }
     }
-    /*public ArrayList<String> dogArray(){
-        dogs.add(Dog1Object.toString());
-        dogs.add(Dog2Object.toString());
-        dogs.add(Dog3Object.toString());
-        return dogs;
-    }*/
-
+    public static void run(Dog[] dogs){
+        for (Dog d : dogs){
+            if (d.getSize().equals("Small")) {
+                exercise = "An energetic 3 miles";
+            }
+            if (d.getSize().equals("Medium")) {
+                exercise = "A brisk 5 miles";
+            }
+            if (d.getSize().equals("Large")) {
+                exercise = "7 miles with intermitten breaks and lots of panting";
+            }
+            System.out.printf("Dog %s has run %s\n", d.getBreed(), exercise);
+            }
+    }
+    public static void sleep(Dog[] dogs){
+        for (Dog d : dogs){
+            if (d.getSize().equals("Small")) {
+                shutEye = "14 - 16 hours today";
+            }
+            if (d.getSize().equals("Medium")) {
+                shutEye = "10 - 14 hours today";
+            }
+            if (d.getSize().equals("Large")) {
+                shutEye = "18 hours today";
+            }
+            System.out.printf("Dog %s has slept %s\n", d.getBreed(), shutEye);
+        }
+    }
+    public static void name(Dog[] dogs){
+        for (Dog d : dogs){
+            if (d.getBreed().equals("Bulldog")) {
+                collar = "Crook";
+            }
+            if (d.getBreed().equals("Beagle")) {
+                collar = "Snoopy";
+            }
+            if (d.getBreed().equals("German Shepard")) {
+                collar = "Biscuit";
+            }
+            System.out.printf("Dog %s has a collar, on the collar it says it's name is %s\n", d.getBreed(), collar);
+        }
+    }
     public static void main(String[] args) {
         Dog Dog1Object = new Dog("Bulldog", "Small", "Light Gray", 5);
         Dog Dog2Object = new Dog("Beagle", "Medium", "Orange", 6);
         Dog Dog3Object = new Dog("German Shepard", "Large", "White & Orange", 6);
-        for(int i = 0; i < dogs.size(); i++) {
-            String dog = dogs.get(i);
-        }
-        Dog1Object.eat("Small");
+        Dog[] dogs = {Dog1Object, Dog2Object, Dog3Object};
+        name(dogs);
+        eat(dogs);
+        run(dogs);
+        sleep(dogs);
     }
 }
