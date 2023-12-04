3f119fee03059fb290cc1e64f724acd9b357b55a
CMENG92 <cmeng723@mycr.redwoods>
2023-12-03 17:19:51 -0800

    Java Assignment 010 complete! -Cameron Meng

---
+++ Food.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Cameron Meng
+ *
+ * @since Version 1.0
+ *
+ */
+public class Food {
+    private double weight; // weight of food is measured in kilograms.
+
+    public Food(double weight){
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
