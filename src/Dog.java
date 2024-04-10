533f1a4eac8a03eec4943673ad57815a083365fd
vathao777 <vathao777@gmail.com>
2024-04-10 11:54:22 -0700

    Finished Assignment 10

--- src/Dog.java
+++ src/Dog.java
@@ -1,66 +1,60 @@
-public class Dog {
-    private String Breed;
-    private String Size;
-    private String Colour;
-    private int Age;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
 
-    public Dog(String breed, String size, String colour, int age) {
-        Breed = breed;
-        Size = size;
-        Colour = colour;
-        Age = age;
-    }
-    public String getBreed() {
-        return Breed;
-    }
-    public void setBreed(String name) {
-        Breed = name;
-    }
-    public String getSize() {
-        return Size;
-    }
-    public void setSize(String size) {
-        Size = size;
-    }
-    public String getColour() {
-        return Colour;
-    }
-    public void setColour(String colour) {
-        Colour = colour;
-    }
-    public int getAge() {
-        return Age;
-    }
-    public void setAge(int age) {
-        Age = age;
+public class Dog {
+    private String breed;
+    private DogSize size;
+    private List<String> color;
+    private int age;
+
+
+
+    private String name;
+
+    public Dog(String breed, DogSize size, String[] color, int age, String name) {
+        this.breed = breed;
+        this.size = size;
+        this.color= new ArrayList<>();
+        for (int i = 0; i< color.length;i++ ){
+            this.color.add(color[i]);
+        }
+        this.age = age;
+        this.name = name;
     }
 
     public void eat(String food) {
-        System.out.println(Breed+" is eating " + food);
+
     }
 
     public void run() {
-        System.out.println(Breed+" is running");
+        Random r = new Random();
+        System.out.printf("Running at speed %d%n", r.nextInt());
     }
 
     public void sleep() {
-        System.out.println(Breed+" is sleeping");
-    }
 
-    public String  setName(String name) {
-       return name;
+    }
+    @Override
+    public String toString(){
+      return  this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public boolean equals(Object o){
+        Dog other = (Dog)o;
+        return  this.size == other.size;
     }
 
 
     public static void main(String[] args) {
-        Dog dog1 = new Dog("Bulldog", "large", "light gray", 5);
-        Dog dog2 = new Dog("Beagle", "large", "orange", 6);
-        Dog dog3 = new Dog("German Shepherd", "large", "white and orange", 6);
+        Dog dog1Object = new Dog("Bulldog", DogSize.MEDIUM, new String[]{"light gray"}, 5,"Fide");
+        Dog dog2Object = new Dog("Beagle", DogSize.MEDIUM, new String[]{"orange"}, 6,"Max");
+        Dog dog3Object = new Dog("German Shepherd",DogSize.XTRALARGE, new String[]{"white", " orange"}, 6, "Rax");
 
-        dog1.eat("bone");
-        dog2.run();
-        dog3.sleep();
-        System.out.println(dog1.setName("Luncky") + " is a " + dog1.getBreed() + ".");
 
     }
 }
